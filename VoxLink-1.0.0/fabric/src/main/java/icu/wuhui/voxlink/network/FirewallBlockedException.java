c3ecbb4c7baedb847cd4fca78f368e8de21e3f70
AUGUHDAR <1943013717@qq.com>
2026-06-30 18:31:57 +0800

    Delete VoxLink-1.0.0 directory

--- VoxLink-1.0.0/fabric/src/main/java/icu/wuhui/voxlink/network/FirewallBlockedException.java
+++
@@ -1,8 +0,0 @@
-package icu.wuhui.voxlink.network;
-
-// 防火墙阻止异常: UDP发包后无任何回包
-public class FirewallBlockedException extends RuntimeException {
-    public FirewallBlockedException(String message) {
-        super(message);
-    }
-}
